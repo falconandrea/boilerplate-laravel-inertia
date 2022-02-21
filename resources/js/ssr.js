6898f2e208c24bf84e448b06227092912bfac54d
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2022-02-21 20:01:07 +0100

    feat: add ssr

---
+++ resources/js/ssr.js
@@ -0,0 +1,21 @@
+import { createSSRApp, h } from 'vue'
+import { renderToString } from '@vue/server-renderer'
+import { createInertiaApp, Link } from '@inertiajs/inertia-vue3'
+import createServer from '@inertiajs/server'
+import Layout from './Layout/App.vue'
+
+createServer((page) => createInertiaApp({
+  page,
+  render: renderToString,
+  resolve: name => {
+    const page = require(`./Pages/${name}`).default
+    if (page.layout === undefined) page.layout = Layout
+    return page
+  },
+  setup ({ app, props, plugin }) {
+    return createSSRApp({
+      render: () => h(app, props)
+    }).use(plugin)
+      .component('Link', Link)
+  }
+}))
