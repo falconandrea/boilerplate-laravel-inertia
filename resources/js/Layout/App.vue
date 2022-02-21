d882a7b4c0b5a5804051253e6abb7c634bde5f9f
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2022-02-21 19:33:17 +0100

    feat: add default layout

---
+++ resources/js/Layout/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <main>
+        <header class="bg-gray-400 p-2">
+            <Link class="text-white underline" href="/">Home</Link>
+        </header>
+        <div class="content">
+            <slot />
+        </div>
+        <footer class="bg-gray-400 mt-4 p-2">
+            &copy; MyApp
+        </footer>
+    </main>
+</template>
